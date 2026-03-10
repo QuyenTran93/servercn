80482ff910ab4ea0e5391df910c856af9e4f901d
Ravinder <ravinder729090@gmail.com>
2026-03-11 00:02:31 +0530

    fix(foundation): remove incorrect nested express-starter directory from prisma mongodb starter

--- packages/templates/node/express/foundation/prisma-mongodb-starter/express-starter/feature/src/configs/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import env from "./env";
-
-const prisma = new PrismaClient({
-  log: env.NODE_ENV === "development" ? ["query", "warn", "error"] : ["error"]
-});
-
-export default prisma;
--- packages/templates/node/express/foundation/prisma-mongodb-starter/express-starter/feature/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import HealthRouter from "../modules/health/health.routes";
-
-const router = Router();
-
-router.use("/health", HealthRouter);
-
-export default router;
